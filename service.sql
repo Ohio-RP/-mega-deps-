de1d8deef3bc9ddaead4243d27a50825fe5fef6d
Santos <55027189+nihonnetwork@users.noreply.github.com>
2025-01-10 00:44:12 +0900

    Update service.sql

--- service.sql
+++ service.sql
@@ -198,15 +198,6 @@ CREATE TABLE IF NOT EXISTS `boats` (
 ALTER TABLE `boats` ADD COLUMN IF NOT EXISTS (`fuel` INT(11) NULL);
 ALTER TABLE `boats` ADD COLUMN IF NOT EXISTS (`condition` INT(11) NULL);
 
-ON DUPLICATE KEY UPDATE
-    `item` = VALUES(`item`),
-    `label` = VALUES(`label`),
-    `limit` = VALUES(`limit`),
-    `can_remove` = VALUES(`can_remove`),
-    `type` = VALUES(`type`),
-    `usable` = VALUES(`usable`),
-    `desc` = VALUES(`desc`);
-	
 CREATE TABLE IF NOT EXISTS `legendaries` (
     `identifier` varchar(50) NOT NULL,
     `charidentifier` int(11) NOT NULL,
